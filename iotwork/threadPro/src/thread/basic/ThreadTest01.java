344a9f0d6b60805d7c09db3f8ab5a7d8729985c4
gorb6593 <gorb6593@gmail.com>
2021-11-10 18:25:45 +0900

    iot

---
+++ iotwork/threadPro/src/thread/basic/ThreadTest01.java
@@ -0,0 +1,42 @@
+package thread.basic;
+
+class ThreadDemo01 extends Thread{
+	ThreadDemo01(String name){
+		super(name);
+	}
+	@Override
+	public void run() {
+		//1부터 20까지 출력하는 코드
+		for (int i = 1; i <= 20; i++) {
+			System.out.print(i+"("+getName()+")");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if(i%5==0) {
+				System.out.println();
+			}
+		}
+	}
+}
+
+public class ThreadTest01 {
+	public static void main(String[] args) {
+		System.out.println("*****프로그램시작*****");
+		ThreadDemo01 t1 = new ThreadDemo01("t1");
+		ThreadDemo01 t2 = new ThreadDemo01("t2");
+		//t1.run(); => 메소드호출(쓰레드프로그래밍 x)
+		t1.start();
+		t2.start();
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("main"+i);
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("*****프로그램종료*****");
+	}
+}
---
+++ iotwork/threadPro/src/thread/basic/ThreadTest02.java
@@ -0,0 +1,56 @@
+package thread.basic;
+
+class DigitThread1 extends Thread{
+	DigitThread1(String name){
+		super(name);
+	}
+	@Override
+	public void run() {
+		//1부터 20까지 출력하는 코드
+		for (int i = 1; i <= 20; i++) {
+			System.out.print(i+"("+getName()+")");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if(i%5==0) {
+				System.out.println();
+			}
+		}
+	}
+}
+class AlphaThread1 extends Thread{
+	AlphaThread1(String name){
+		super(name);
+	}
+	@Override
+	public void run() {
+		for (char i = 'A'; i <= 'Z'; i++) {
+			System.out.print(i);
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+public class ThreadTest02 {
+	public static void main(String[] args) {
+		System.out.println("시작");		
+		DigitThread1 a1 = new DigitThread1("a1");
+		AlphaThread1 d1 = new AlphaThread1("d1");
+		a1.start();
+		d1.start();
+		System.out.println("종료");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("main"+i);
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
